de458862190d5c483fe230ecafce7d099190809f
Joab Felippe <80433162+JoabFelippx@users.noreply.github.com>
2025-10-20 09:35:47 -0300

    Update input_handler.py

--- simulation/input_handler.py
+++ simulation/input_handler.py
@@ -13,17 +13,17 @@ def handle_input(keys): # Retorna um dicionário de comandos baseado nas teclas 
     if keys[pygame.K_s]: move_vector[1] -= 1.0
     if keys[pygame.K_a]: move_vector[0] -= 1.0
     if keys[pygame.K_d]: move_vector[0] += 1.0
+    
 
     if np.linalg.norm(move_vector) > 0: # 
         commands['walk_direction'] = math.atan2(move_vector[0], move_vector[1])
 
     max_angle = config.BODY_ROTATION_MAX_ANGLE_DEG
-    if keys[pygame.K_UP]: commands['body_rotation'][1] = -max_angle
-    if keys[pygame.K_DOWN]: commands['body_rotation'][1] = max_angle
-    if keys[pygame.K_LEFT]: commands['body_rotation'][0] = -max_angle
-    if keys[pygame.K_RIGHT]: commands['body_rotation'][0] = max_angle
+    if keys[pygame.K_UP]: commands['body_rotation'][1] = max_angle    
+    if keys[pygame.K_DOWN]: commands['body_rotation'][1] = -max_angle 
+    if keys[pygame.K_LEFT]: commands['body_rotation'][0] = max_angle    
+    if keys[pygame.K_RIGHT]: commands['body_rotation'][0] = -max_angle
     if keys[pygame.K_q]: commands['body_rotation'][2] = max_angle
     if keys[pygame.K_e]: commands['body_rotation'][2] = -max_angle
         
     return commands
-
